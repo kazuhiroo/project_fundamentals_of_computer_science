a8496dc33e242f953e341b2a8da82edad3705463
kazuhiroo <katowski.antoni@gmail.com>
2024-01-04 18:01:15 +0100

    Uploading background

--- game/Gra.h
+++ game/Gra.h
@@ -21,8 +21,24 @@ private:
 	//Stworzenie wskaŸnika zawieraj¹cego okno - dynamiczne zarz¹dzanie gr¹
 	RenderWindow* window;
 	Event event;
+
+
+
 	//Zasoby gry
 	Font font;
+	Texture bg;
+	Sprite background;
+
+
+
+	//ZMIENNE
+	bool exit = false;
+	
+
+
+
+
+
 
 
 	
@@ -33,7 +49,10 @@ private:
 	{
 		this->window = new RenderWindow(VideoMode(800, 900), "arcade");
 		this->window->setFramerateLimit(144);
+		
 	}
+
+
 	//Zdarzenie: zamkniêcie okna X
 	void pollEvents()
 	{
@@ -46,15 +65,40 @@ private:
 			}
 		}
 	}
+
+
 	//Wczytanie fontu
 	void game_font()
 	{
 		if (this->font.loadFromFile("Fonts/PixelEmulator-xq08.ttf"))
 		{
-			std::cout << "GAME::GAME_FONT::Font uploaded successfully";
+			std::cout << "GAME::RESOURCES::Font uploaded successfully" << std::endl;
+		}
+		if (this->font.loadFromFile("Fonts/PixelEmulator-xq08.ttf" ) == false)
+		{
+			std::cout << "GAME::RESOURCES::Font uploading unsuccessful" << std::endl;
 		}
 	}
-	
+
+
+
+	//Wczytanie grafiki i t³a
+	void game_bg()
+	{
+		if (this->bg.loadFromFile("Bg/bg.png"))
+		{
+			std::cout << "GAME::RESOURCES::Backgroud uploaded successfully" << std::endl;
+		}
+		if (this->bg.loadFromFile("Bg/bg.png") == false)
+		{
+			std::cout << "GAME::RESOURCES::Backgroud uploading unsuccessful"<< std::endl;
+		}
+	}
+	void create_background()
+	{
+		
+		this->background.setTexture(this->bg);
+	}
 public:
 	//Konstruktor/Destruktor
 	Game()
@@ -62,6 +106,8 @@ public:
 		//Konstruktor tworzy okno przy utworzeniu obiektu
 		this->create_window();
 		this->game_font();
+		this->game_bg();
+		this->create_background();
 	}
 
 	~Game()
@@ -87,9 +133,12 @@ public:
 	void render()
 	{
 		this->window->clear();
-
+		this->window->draw(this->background);
 		this->window->display();
 	}
 
 };
+
+
+
 
